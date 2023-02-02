a0f032761710105bb1c55a2e21f9a673d84cceb7
chethan1901 <chethanreddy.akkati123@gmail.com>
2023-02-02 17:25:20 +0530

    upload and download file from onedrive

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import OneDriveUpload from "./Components/Upload/Upload";
+import OneDriveDownload from "./Components/Download/Download";
+
+
+function App() {
+  return (
+    <div className="App">
+      <OneDriveUpload />
+      <OneDriveDownload />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Download/Download.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+const OneDriveDownload = () => {
+  const [fileId, setFileId] = useState(null);
+  const [photo,setPhoto] = useState([])
+
+  const handleFilePick = async () => {
+    const accessToken = "EwCAA8l6BAAUkj1NuJYtTVha%2bMogk%2bHEiPbQo04AAWBTj/AEtYYwE3IxB10bc6L8J9yBQK%2beLjeVrHub5MpMNXyR2TyEfxCNCcWZPM7fn/aiLnr2r7Yn1YiFQQY8zHVMgiu%2bNGYSjBRjtjd10hE2uWnIYR12sZ1wzoERL8FvJkeocNhJo%2bHW9MG3/1NNKhNJX6KtE/AZ4ydHLVLTiXXPFyigm8XGVsFRWY7G81jEISveUfsZrU1PbeRKnjfhxt2xT1vYL2/dyGLT/NzYQ2H8etrclYujWT7ggtjpL4pd2dOVZJuZUVueALjm%2bN5Kv7DwjjHGfFyS8B5F/6XHzFZe2R0eE/lDqrBYzW6V5fPjqeNKZLbcSE1b1TugY6COKYkDZgAACGmat9sx461uUAK6KlObaTeqxhzWkgP8u0EnrkjjwoJhYElgBfhCk3yFCiqdW8R%2bbZBi6mxmN/WlYOwsZcmks0OtxMzxAr1IUxMVL%2bhBUMVxVbIV1DhypCOnNTKALrexFQFweDeqVA72lCZzUX5itF4bjOSUhzMXqPQAY//5hyAyc3MGlGwXvhVSQNE9U75jYZqkGpRUaD3J2RV3sZJEYvkW3zgArEMPKNEpQixETS/d5P0vwCt4IL3tZoCtYa2BkfYg/T8gvsQ8OyRq6SUesMXZeGyUiLfZ8orAJMiyO27HE30OMF10u36ZfdbfJNoxsUNCeclfVoqW4YALsvE2LFJ3LZZzart1WtE4WxdTgWuzgdcvp8zrQTTVQGHfeiK88Vs1BNW62Z6p7qdl3fYjcJhLMtFPmsfYFDudrqh%2bd0dlm23M%2boJnhV39zdeThVA3yqnZVyYF1iFMZ%2b%2baCuZHLzpbsTQks2CKrerpUqzgxvL0jW3I%2bYy4WGIjFS/asXCUk0oS2tnPP5Aw4dwzvJ4X%2b70XU9iufIDi/E8TQX7TuML13kthcPevIPpReBMICMGlc%2b5EEq0HNAVD6LTBeJjfl8HO%2bVRApIYr3KxumTFGMFuLJu7Z9Qux0rB%2bt/BLvdFEknEhBk1D5XCLPDTh5F1AdVVO%2bPdWLfCeInTtW8njlt/UM8694E%2bV7SlhnhIual6NpAQnSTH%2bCW5tClQZl8%2bSNFuMvnmxTxnwIkTjzQXAwUyXcu1r7FrEpYvBraNlJlE9a6ps7ROwbD7uPjH1BRHezR5uUUQ5DXnW%2bNfalwI%3d";
+    const url = `https://graph.microsoft.com/v1.0/me/drive/root/children`;
+
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const files = await response.json();
+      setPhoto(files.value.photo)
+      setFileId(files.value[0].id);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDownload = async () => {
+    if (!fileId) return;
+
+    const accessToken = "EwCAA8l6BAAUkj1NuJYtTVha%2bMogk%2bHEiPbQo04AAWBTj/AEtYYwE3IxB10bc6L8J9yBQK%2beLjeVrHub5MpMNXyR2TyEfxCNCcWZPM7fn/aiLnr2r7Yn1YiFQQY8zHVMgiu%2bNGYSjBRjtjd10hE2uWnIYR12sZ1wzoERL8FvJkeocNhJo%2bHW9MG3/1NNKhNJX6KtE/AZ4ydHLVLTiXXPFyigm8XGVsFRWY7G81jEISveUfsZrU1PbeRKnjfhxt2xT1vYL2/dyGLT/NzYQ2H8etrclYujWT7ggtjpL4pd2dOVZJuZUVueALjm%2bN5Kv7DwjjHGfFyS8B5F/6XHzFZe2R0eE/lDqrBYzW6V5fPjqeNKZLbcSE1b1TugY6COKYkDZgAACGmat9sx461uUAK6KlObaTeqxhzWkgP8u0EnrkjjwoJhYElgBfhCk3yFCiqdW8R%2bbZBi6mxmN/WlYOwsZcmks0OtxMzxAr1IUxMVL%2bhBUMVxVbIV1DhypCOnNTKALrexFQFweDeqVA72lCZzUX5itF4bjOSUhzMXqPQAY//5hyAyc3MGlGwXvhVSQNE9U75jYZqkGpRUaD3J2RV3sZJEYvkW3zgArEMPKNEpQixETS/d5P0vwCt4IL3tZoCtYa2BkfYg/T8gvsQ8OyRq6SUesMXZeGyUiLfZ8orAJMiyO27HE30OMF10u36ZfdbfJNoxsUNCeclfVoqW4YALsvE2LFJ3LZZzart1WtE4WxdTgWuzgdcvp8zrQTTVQGHfeiK88Vs1BNW62Z6p7qdl3fYjcJhLMtFPmsfYFDudrqh%2bd0dlm23M%2boJnhV39zdeThVA3yqnZVyYF1iFMZ%2b%2baCuZHLzpbsTQks2CKrerpUqzgxvL0jW3I%2bYy4WGIjFS/asXCUk0oS2tnPP5Aw4dwzvJ4X%2b70XU9iufIDi/E8TQX7TuML13kthcPevIPpReBMICMGlc%2b5EEq0HNAVD6LTBeJjfl8HO%2bVRApIYr3KxumTFGMFuLJu7Z9Qux0rB%2bt/BLvdFEknEhBk1D5XCLPDTh5F1AdVVO%2bPdWLfCeInTtW8njlt/UM8694E%2bV7SlhnhIual6NpAQnSTH%2bCW5tClQZl8%2bSNFuMvnmxTxnwIkTjzQXAwUyXcu1r7FrEpYvBraNlJlE9a6ps7ROwbD7uPjH1BRHezR5uUUQ5DXnW%2bNfalwI%3d";
+    const url = `https://graph.microsoft.com/v1.0/me/drive/items/${fileId}/content`;
+
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const blob = await response.blob();
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = `file.jpg`;
+      link.click();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(".........",photo)
+  return (
+    <>
+    <div>
+      <p></p>
+      <button onClick={handleFilePick}>Pick file</button>
+      <button onClick={handleDownload}>Download</button>
+      <p></p>
+    </div>
+    <div> {photo.map((ele,id) => {
+      return (
+        <p>${id}{ele.webUrl}</p>
+      )})}</div>
+      </>
+  );
+};
+
+export default OneDriveDownload;
